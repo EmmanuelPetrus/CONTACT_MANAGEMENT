c32783eba2202e5ca293cd70961b5d853e3331c8
Emmanuel Peter <Peterojonilemi@gmail.com>
2022-09-16 17:38:28 +0100

    added color to beautify

--- CONTACT.c
+++ CONTACT.c
@@ -11,7 +11,7 @@ int main()
 
 main:
     system("cls"); /* ************Main menu ***********************  */
-    system("color fc");
+    system("color 2d");
     printf("\n\t **** Welcome to Contact Management System ****");
 
     printf("\n\n\n\t\t\tMAIN MENU\n\t\t=====================\n\t\t[1] Add a new Contact\n\t\t[2] List all Contacts\n\t\t[3] Search for contact\n\t\t[4] Edit a Contact\n\t\t[5] Delete a Contact\n\t\t[0] Exit\n\t\t=================\n\t\t");
@@ -40,6 +40,7 @@ main:
     case 1:
 
         system("cls");
+        system("color 08");
         addNewContact(pCon);
         break;
 
@@ -47,6 +48,7 @@ main:
     case 2:
 
         system("cls");
+        system("color 70");
         listContact(pCon);
         break;
 
@@ -55,18 +57,21 @@ main:
     case 3:
 
         system("cls");
+        system("color fc");
         searchContact();
         break;
 
         /* *********************edit contacts************************/
 
     case 4:
+        system("color 71");
         editContact();
         break;
 
         /* ********************delete contacts**********************/
 
     case 5:
+        system("color a7");
         deleteContact(pCon);
         break;
 
